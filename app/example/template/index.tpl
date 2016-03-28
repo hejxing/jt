bb255919c47ecf83982b835f0436f38e0f512286
何渐兴 <ax@jentian.com>
2016-03-28 16:10:18 +0800

    调整目录结构

--- app/example/template/index.tpl
+++
@@ -1,5 +0,0 @@
-{{extends file='layout\html.tpl'}}
-
-{{block name="body"}}
-    <h1>{{$action}} {{$name}}!</h1>
-{{/block}}
--- app/project/index_production.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * 项目入口文件
- * 通过服务器的URLRewrite模块将所有的请求转到该文件
- */
-require '../../jt/Bootstrap.php';
-
-\jt\Bootstrap::boot();
