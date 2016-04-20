a177110d92689719a86353c2abebe15aa8bbfacb
何渐兴 <ax@jentian.com>
2016-04-20 16:05:34 +0800

    修改调试入口，将调试相关的工作全部移入\jt\utils\Debug中

--- lib/test/test_entrance.php
+++ lib/test/test_entrance.php
@@ -4,23 +4,6 @@
  * 通过服务器的URLRewrite模块将所有的请求转到该文件
  */
 $root = realpath(__DIR__ . '/../..');
-require $root . '/Bootstrap.php';
+require $root . '/utils/Debug.php';
 
-$pwd = $_SERVER['PWD'];
-
-$pos = strpos($pwd, '/app/');
-if ($pos) {
-    $root = implode('/', array_slice(explode('/', $pwd, 6), 0, 5));
-}
-
-$testClass = $_SERVER['argv'][4];
-$testFile = $_SERVER['argv'][5];
-
-$projectRoot = explode(str_replace('\\',DIRECTORY_SEPARATOR, $testClass), $testFile)[0];
-$projectRoot = substr($projectRoot, 0 ,-1);
-//\jt\Bootstrap::test($root);
-if (file_exists($projectRoot . '/test_entrance.php')) {
-    require $projectRoot . '/test_entrance.php';
-}else{
-    \jt\Bootstrap::test($projectRoot);
-}
+\jt\utils\Debug::entrance($root);
